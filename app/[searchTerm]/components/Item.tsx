67ffe10dbbbb87ccc4b426ce599f213bf9c0f4c8
sabin149 <dangalsabin149@gmail.com>
2023-12-03 13:26:14 +0545

    fix: added the Image from the next

--- app/[searchTerm]/components/Item.tsx
+++ app/[searchTerm]/components/Item.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 type Props = {
@@ -24,7 +25,7 @@ export default function Item({ result }: Props) {
     <article className="m-4 max-w-lg">
       <div className="flex flex-row gap-4">
         <div className="flex flex-col justify-center">
-          <img
+          <Image
             src={result.thumbnail.source}
             alt={result.title}
             width={result.thumbnail.width}
